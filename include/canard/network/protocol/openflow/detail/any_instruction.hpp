9a40f7bd845d41025a94bb05769e37a0b00faa88
amedama41 <kamo.tanabota@gmail.com>
2016-07-31 08:31:24 +0900

    change equality operator semantics of any_instruction

--- include/canard/network/protocol/openflow/detail/any_instruction.hpp
+++ include/canard/network/protocol/openflow/detail/any_instruction.hpp
@@ -1,10 +1,13 @@
 #ifndef CANARD_NETWORK_OPENFLOW_DETAIL_ANY_INSTRUCTION_HPP
 #define CANARD_NETWORK_OPENFLOW_DETAIL_ANY_INSTRUCTION_HPP
 
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <type_traits>
 #include <utility>
+#include <boost/mpl/contains.hpp>
+#include <boost/mpl/size.hpp>
 #include <boost/operators.hpp>
 #include <boost/variant/apply_visitor.hpp>
 #include <boost/variant/get.hpp>
@@ -32,7 +35,15 @@ namespace detail {
         using instruction_variant
             = typename boost::make_variant_over<instruction_type_list>::type;
 
+        template <class Instruction>
+        using containable_if_t = typename std::enable_if<
+            boost::mpl::contains<instruction_type_list, Instruction>::value
+        >::type;
+
     public:
+        static constexpr std::size_t number_of_types
+            = boost::mpl::size<instruction_type_list>::type::value;
+
         template <
               class Instruction
             , typename std::enable_if<
@@ -69,6 +80,12 @@ namespace detail {
         {
             auto visitor = detail::length_visitor{};
             return boost::apply_visitor(visitor, variant_);
+        }
+
+        auto index() const noexcept
+            -> std::size_t
+        {
+            return variant_.which();
         }
 
         template <class Container>
@@ -108,6 +125,62 @@ namespace detail {
             -> bool
         {
             return lhs.variant_ == rhs.variant_;
+        }
+
+        template <class Instruction, class = containable_if_t<Instruction>>
+        friend auto operator==(
+                any_instruction const& lhs, Instruction const& rhs)
+            -> bool
+        {
+            if (auto const instruction
+                    = boost::get<Instruction>(std::addressof(lhs.variant_))) {
+                return *instruction == rhs;
+            }
+            return false;
+        }
+
+        template <class Instruction, class = containable_if_t<Instruction>>
+        friend auto operator==(
+                Instruction const& lhs, any_instruction const& rhs)
+            -> bool
+        {
+            if (auto const instruction
+                    = boost::get<Instruction>(std::addressof(rhs.variant_))) {
+                return lhs == *instruction;
+            }
+            return false;
+        }
+
+        friend auto equivalent(
+                any_instruction const& lhs, any_instruction const& rhs) noexcept
+            -> bool
+        {
+            auto visitor = detail::equivalent_visitor{};
+            return boost::apply_visitor(visitor, lhs.variant_, rhs.variant_);
+        }
+
+        template <class Instruction, class = containable_if_t<Instruction>>
+        friend auto equivalent(
+                any_instruction const& lhs, Instruction const& rhs) noexcept
+            -> bool
+        {
+            if (auto const instruction
+                    = boost::get<Instruction>(std::addressof(lhs.variant_))) {
+                return equivalent(*instruction, rhs);
+            }
+            return false;
+        }
+
+        template <class Instruction, class = containable_if_t<Instruction>>
+        friend auto equivalent(
+                Instruction const& lhs, any_instruction const& rhs) noexcept
+            -> bool
+        {
+            if (auto const instruction
+                    = boost::get<Instruction>(std::addressof(rhs.variant_))) {
+                return equivalent(lhs, *instruction);
+            }
+            return false;
         }
 
         template <class T, class Decoder>
