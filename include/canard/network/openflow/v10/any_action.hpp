0ec14af32a5cb92aebae03735c73a8f2c0f64a47
amedama41 <kamo.tanabota@gmail.com>
2016-08-14 19:19:00 +0900

    control any_action template instantiation for v10 any_action

--- include/canard/network/openflow/v10/any_action.hpp
+++ include/canard/network/openflow/v10/any_action.hpp
@@ -1,6 +1,8 @@
 #ifndef CANARD_NET_OFP_V10_ANY_ACTION_HPP
 #define CANARD_NET_OFP_V10_ANY_ACTION_HPP
 
+#include <canard/network/openflow/detail/config.hpp>
+
 #include <canard/network/openflow/detail/any_action.hpp>
 #include <canard/network/openflow/v10/actions.hpp>
 #include <canard/network/openflow/v10/decoder/action_decoder.hpp>
@@ -31,5 +33,23 @@ namespace v10 {
 } // namespace ofp
 } // namespace net
 } // namespace canard
+
+#if !defined(CANARD_NET_OFP_HEADER_ONLY)
+# if defined(CANARD_NET_OFP_USE_EXPLICIT_INSTANTIATION)
+
+namespace canard {
+namespace net {
+namespace ofp {
+namespace detail {
+
+  extern template class any_action<ofp::v10::action_decoder>;
+
+} // namespace detail
+} // namespace ofp
+} // namespace net
+} // namespace canard
+
+# endif
+#endif
 
 #endif // CANARD_NET_OFP_V10_ANY_ACTION_HPP
