083f50694803d0cfef0319f4c4c5394be2eb3c00
amedama41 <kamo.tanabota@gmail.com>
2016-08-28 12:34:44 +0900

    rewrite v10 packet_out type

--- include/canard/network/openflow/v10/message/packet_out.hpp
+++ include/canard/network/openflow/v10/message/packet_out.hpp
@@ -6,10 +6,13 @@
 #include <memory>
 #include <stdexcept>
 #include <utility>
+#include <boost/operators.hpp>
+#include <boost/range/algorithm/equal.hpp>
 #include <boost/range/iterator_range.hpp>
 #include <canard/network/openflow/binary_data.hpp>
 #include <canard/network/openflow/detail/decode.hpp>
 #include <canard/network/openflow/detail/encode.hpp>
+#include <canard/network/openflow/detail/memcmp.hpp>
 #include <canard/network/openflow/get_xid.hpp>
 #include <canard/network/openflow/v10/action_list.hpp>
 #include <canard/network/openflow/v10/detail/basic_openflow_message.hpp>
@@ -24,11 +27,10 @@ namespace messages {
 
     class packet_out
         : public v10_detail::basic_openflow_message<packet_out>
+        , private boost::equality_comparable<packet_out>
     {
-        static constexpr std::uint16_t base_pkt_out_size
-            = sizeof(v10_detail::ofp_packet_out);
-
     public:
+        using raw_ofp_type = v10_detail::ofp_packet_out;
         using data_type = binary_data::pointer_type;
 
         static constexpr protocol::ofp_type message_type
@@ -45,13 +47,13 @@ namespace messages {
                         protocol::OFP_VERSION
                       , message_type
                       , std::uint16_t(
-                                base_pkt_out_size
+                                sizeof(raw_ofp_type)
                               + actions.length() + data.size())
                       , xid
                   }
                 , buffer_id
                 , in_port
-                , actions.length()
+                , std::uint16_t(actions.length())
               }
             , actions_(std::move(actions))
             , data_(std::move(data).data())
@@ -72,7 +74,9 @@ namespace messages {
         packet_out(std::uint32_t const buffer_id
                  , action_list actions
                  , std::uint32_t const xid = get_xid())
-            : packet_out{buffer_id, protocol::OFPP_NONE, std::move(actions), xid}
+            : packet_out{
+                buffer_id, protocol::OFPP_NONE, std::move(actions), xid
+              }
         {
         }
 
@@ -108,18 +112,16 @@ namespace messages {
 
         packet_out(packet_out&& other)
             : packet_out_(other.packet_out_)
-            , actions_(std::move(other.actions_))
+            , actions_(other.extract_actions())
             , data_(std::move(other.data_))
         {
-            other.packet_out_.header.length = base_pkt_out_size;
-            other.packet_out_.actions_len = 0;
+            other.packet_out_.header.length = sizeof(raw_ofp_type);
         }
 
         auto operator=(packet_out const& other)
             -> packet_out&
         {
-            auto tmp = other;
-            return operator=(std::move(tmp));
+            return operator=(packet_out{other});
         }
 
         auto operator=(packet_out&& other)
@@ -162,16 +164,34 @@ namespace messages {
             return actions_;
         }
 
+        auto extract_actions()
+            -> action_list
+        {
+            auto actions = action_list{};
+            actions.swap(actions_);
+            packet_out_.header.length -= packet_out_.actions_len;
+            packet_out_.actions_len = 0;
+            return actions;
+        }
+
         auto frame() const noexcept
             -> boost::iterator_range<unsigned char const*>
         {
             return boost::make_iterator_range_n(data_.get(), frame_length());
         }
 
+        auto extract_frame()
+            -> binary_data
+        {
+            auto data = binary_data{std::move(data_), frame_length()};
+            packet_out_.header.length -= data.size();
+            return data;
+        }
+
         auto frame_length() const noexcept
             -> std::uint16_t
         {
-            return length() - base_pkt_out_size - actions_length();
+            return length() - sizeof(raw_ofp_type) - actions_length();
         }
 
         template <class Container>
@@ -188,10 +208,9 @@ namespace messages {
         static auto decode(Iterator& first, Iterator last)
             -> packet_out
         {
-            auto const pkt_out
-                = detail::decode<v10_detail::ofp_packet_out>(first, last);
+            auto const pkt_out = detail::decode<raw_ofp_type>(first, last);
             last = std::next(
-                    first, pkt_out.header.length - base_pkt_out_size);
+                    first, pkt_out.header.length - sizeof(raw_ofp_type));
             if (std::distance(first, last) < pkt_out.actions_len) {
                 throw std::runtime_error{"invalid actions length"};
             }
@@ -212,13 +231,16 @@ namespace messages {
             if (header.type != message_type) {
                 throw std::runtime_error{"invalid message type"};
             }
-            if (header.length < base_pkt_out_size) {
+            if (header.length < sizeof(raw_ofp_type)) {
                 throw std::runtime_error{"invalid length"};
             }
         }
 
+        friend auto operator==(packet_out const&, packet_out const&) noexcept
+            -> bool;
+
     private:
-        packet_out(v10_detail::ofp_packet_out const& pkt_out
+        packet_out(raw_ofp_type const& pkt_out
                  , action_list&& actions
                  , data_type&& data)
             : packet_out_(pkt_out)
@@ -227,11 +249,26 @@ namespace messages {
         {
         }
 
+        auto equal_impl(packet_out const& rhs) const noexcept
+            -> bool
+        {
+            return detail::memcmp(packet_out_, rhs.packet_out_)
+                && actions_ == rhs.actions_
+                && boost::equal(frame(), rhs.frame());
+        }
+
     private:
-        v10_detail::ofp_packet_out packet_out_;
+        raw_ofp_type packet_out_;
         action_list actions_;
         data_type data_;
     };
+
+    inline auto operator==(
+            packet_out const& lhs, packet_out const& rhs) noexcept
+        -> bool
+    {
+        return lhs.equal_impl(rhs);
+    }
 
 } // namespace messages
 } // namespace v10
