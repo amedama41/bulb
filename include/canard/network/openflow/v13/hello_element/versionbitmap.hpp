3cca6d337053c2449524838838c93822695e4fea
amedama41 <kamo.tanabota@gmail.com>
2016-09-09 13:53:15 +0900

    rewrite v13 versionbitmap hello element type

--- include/canard/network/openflow/v13/hello_element/versionbitmap.hpp
+++ include/canard/network/openflow/v13/hello_element/versionbitmap.hpp
@@ -8,12 +8,15 @@
 #include <stdexcept>
 #include <utility>
 #include <vector>
+#include <boost/algorithm/cxx11/all_of.hpp>
 #include <boost/endian/conversion.hpp>
+#include <boost/operators.hpp>
 #include <boost/range/adaptor/reversed.hpp>
 #include <boost/range/algorithm/find_if.hpp>
 #include <boost/range/algorithm/for_each.hpp>
 #include <canard/network/openflow/detail/decode.hpp>
 #include <canard/network/openflow/detail/encode.hpp>
+#include <canard/network/openflow/detail/memcmp.hpp>
 #include <canard/network/openflow/detail/padding.hpp>
 #include <canard/network/openflow/v13/detail/byteorder.hpp>
 #include <canard/network/openflow/v13/detail/length_utility.hpp>
@@ -26,63 +29,109 @@ namespace v13 {
 namespace hello_elements {
 
   class versionbitmap
+    : private boost::equality_comparable<versionbitmap>
   {
   public:
-    static protocol::ofp_hello_elem_type const hello_element_type
+    using raw_ofp_type = v13_detail::ofp_hello_elem_versionbitmap;
+    using bitmaps_type = std::vector<std::uint32_t>;
+
+  private:
+    using bitmap_type = bitmaps_type::value_type;
+    static constexpr std::size_t bitmap_bits
+      = std::numeric_limits<bitmap_type>::digits;
+
+  public:
+    static constexpr protocol::ofp_hello_elem_type hello_element_type
       = protocol::OFPHET_VERSIONBITMAP;
 
-    explicit versionbitmap(std::vector<std::uint32_t> bitmaps)
+    explicit versionbitmap(bitmaps_type bitmaps)
       : versionbitmap_{
             hello_element_type
-          , std::uint16_t(
-              sizeof(versionbitmap_) + bitmaps.size() * sizeof(bitmaps[0]))
+          , calc_length(bitmaps)
         }
       , bitmaps_{std::move(bitmaps)}
     {
-      using boost::adaptors::reversed;
-      auto const it = boost::find_if(
-            bitmaps_ | reversed
-          , [](std::uint32_t const bitmap) { return bitmap != 0; });
-      bitmaps_.erase(it.base(), bitmaps_.end());
-      if (bitmaps_.empty()) {
-        throw std::runtime_error{"versionbitmap nust not be empty"};
-      }
     }
 
-    auto type() const
+    versionbitmap(versionbitmap const&) = default;
+
+    versionbitmap(versionbitmap&& other)
+      : versionbitmap_(other.versionbitmap_)
+      , bitmaps_(other.extract_bitmaps())
+    {
+    }
+
+    auto operator=(versionbitmap const& other)
+      -> versionbitmap&
+    {
+      return operator=(versionbitmap{other});
+    }
+
+    auto operator=(versionbitmap&& other)
+      -> versionbitmap&
+    {
+      auto tmp = std::move(other);
+      std::swap(versionbitmap_, tmp.versionbitmap_);
+      bitmaps_.swap(tmp.bitmaps_);
+      return *this;
+    }
+
+    static constexpr auto type() noexcept
       -> protocol::ofp_hello_elem_type
     {
-      return protocol::ofp_hello_elem_type(versionbitmap_.type);
+      return hello_element_type;
     }
 
-    auto length() const
+    auto length() const noexcept
       -> std::uint16_t
     {
       return versionbitmap_.length;
     }
 
-    auto support(std::uint8_t const version) const
+    auto bitmaps() const noexcept
+      -> bitmaps_type const&
+    {
+      return bitmaps_;
+    }
+
+    auto extract_bitmaps()
+      -> bitmaps_type
+    {
+      auto bitmaps = bitmaps_type{};
+      bitmaps.swap(bitmaps_);
+      versionbitmap_.length = sizeof(raw_ofp_type);
+      return bitmaps;
+    }
+
+    auto support(std::uint8_t const version) const noexcept
       -> bool
     {
-      auto const bitmap_bits = std::size_t(32);
-      auto const array_index = std::size_t(version / bitmap_bits);
-      auto const bit_index = std::size_t(version % bitmap_bits);
-      if (bitmaps_.size() < array_index) {
+      auto const index = std::size_t(version / bitmap_bits);
+      auto const shift = std::size_t(version % bitmap_bits);
+      if (bitmaps_.size() <= index) {
         return false;
       }
-      return (std::uint32_t(1) << bit_index) & bitmaps_[array_index];
+      return (bitmap_type{1} << shift) & bitmaps_[index];
     }
 
     auto max_support_version() const
       -> std::uint8_t
     {
-      auto bit_index = std::size_t{0};
-      auto bitmap = bitmaps_.back();
-      while (bitmap != 0) {
-        bitmap >>= 1;
-        ++bit_index;
+      auto const rbitmaps = bitmaps_ | boost::adaptors::reversed;
+      auto const rit
+        = boost::find_if(rbitmaps, [](bitmap_type b) { return b != 0; });
+      if (rit == rbitmaps.end()) {
+        throw std::runtime_error{"no valid bitmaps"};
       }
-      return bit_index - 1 + (bitmaps_.size() - 1) * 32;
+
+      auto const it = std::prev(rit.base());
+      auto bitmap = *it;
+      auto shift = std::size_t{0};
+      while (bitmap != 1) {
+        bitmap >>= 1;
+        ++shift;
+      }
+      return shift + std::distance(bitmaps_.begin(), it) * bitmap_bits;
     }
 
     template <class Container>
@@ -90,44 +139,26 @@ namespace hello_elements {
       -> Container&
     {
       detail::encode(container, versionbitmap_);
-      boost::for_each(bitmaps_, [&](std::uint32_t const bitmap) {
+      boost::for_each(bitmaps_, [&](bitmap_type bitmap) {
           detail::encode(container, bitmap);
       });
       return detail::encode_byte_array(
           container, detail::padding, v13_detail::padding_length(length()));
     }
 
-  private:
-    versionbitmap(
-          v13_detail::ofp_hello_elem_versionbitmap const& versionbitmap
-        , std::vector<std::uint32_t> bitmaps)
-      : versionbitmap_(versionbitmap)
-      , bitmaps_(std::move(bitmaps))
-    {
-      if (versionbitmap_.type != hello_element_type) {
-        throw 1;
-      }
-      if (versionbitmap_.length
-          != sizeof(versionbitmap_) + bitmaps_.size() * sizeof(std::uint32_t)) {
-        throw 1;
-      }
-    }
-
-  public:
     template <class Iterator>
     static auto decode(Iterator& first, Iterator last)
       -> versionbitmap
     {
-      auto const vbitmap
-        = detail::decode<v13_detail::ofp_hello_elem_versionbitmap>(first, last);
+      auto const vbitmap = detail::decode<raw_ofp_type>(first, last);
 
-      auto bitmaps = std::vector<std::uint32_t>(
-          (vbitmap.length - sizeof(vbitmap)) / sizeof(std::uint32_t));
+      auto const bitmaps_length = vbitmap.length - sizeof(raw_ofp_type);
+      auto bitmaps = bitmaps_type(bitmaps_length / sizeof(bitmap_type));
       std::copy_n(
-            first, bitmaps.size() * sizeof(std::uint32_t)
+            first, bitmaps_length
           , reinterpret_cast<unsigned char*>(&bitmaps[0]));
-      std::advance(first, bitmaps.size() * sizeof(std::uint32_t));
-      boost::for_each(bitmaps, [](std::uint32_t& bitmap) {
+      std::advance(first, bitmaps_length);
+      boost::for_each(bitmaps, [](bitmap_type& bitmap) {
           boost::endian::big_to_native_inplace(bitmap);
       });
 
@@ -136,10 +167,73 @@ namespace hello_elements {
       return versionbitmap{vbitmap, std::move(bitmaps)};
     }
 
+    template <class Validator>
+    void validate(Validator) const
+    {
+      if (bitmaps_.empty()) {
+        throw std::runtime_error{"bitmaps is never empty"};
+      }
+      if (boost::algorithm::all_of(
+            bitmaps_, [](bitmap_type b) { return b == 0; })) {
+        throw std::runtime_error{
+          "bitmaps must include at least one non zero bitmap"
+        };
+      }
+    }
+
+    static void validate_header(v13_detail::ofp_hello_elem_header const& header)
+    {
+      if (header.type != hello_element_type) {
+        throw std::runtime_error{"type is not versionbitmap"};
+      }
+      if (header.length < sizeof(raw_ofp_type)) {
+        throw std::runtime_error{"versionbitmap length is too small"};
+      }
+      if ((header.length - sizeof(raw_ofp_type)) % sizeof(bitmap_type) != 0) {
+        throw std::runtime_error{"versionbitmap length is invalid"};
+      }
+    }
+
+    friend auto operator==(versionbitmap const&, versionbitmap const&) noexcept
+      -> bool;
+
   private:
-    v13_detail::ofp_hello_elem_versionbitmap versionbitmap_;
-    std::vector<std::uint32_t> bitmaps_;
+    versionbitmap(raw_ofp_type const& versionbitmap, bitmaps_type&& bitmaps)
+      : versionbitmap_(versionbitmap)
+      , bitmaps_(std::move(bitmaps))
+    {
+    }
+
+    auto equal_impl(versionbitmap const& rhs) const noexcept
+      -> bool
+    {
+      return detail::memcmp(versionbitmap_, rhs.versionbitmap_)
+          && bitmaps_ == rhs.bitmaps_;
+    }
+
+    static auto calc_length(bitmaps_type const& bitmaps)
+      -> std::uint16_t
+    {
+      constexpr auto max_bitmaps_size
+        = (std::numeric_limits<std::uint16_t>::max() - sizeof(raw_ofp_type))
+        / sizeof(bitmap_type);
+      if (bitmaps.size() > max_bitmaps_size) {
+        throw std::runtime_error{"invalid bitmaps size"};
+      }
+      return sizeof(raw_ofp_type) + bitmaps.size() * sizeof(bitmap_type);
+    }
+
+  private:
+    raw_ofp_type versionbitmap_;
+    bitmaps_type bitmaps_;
   };
+
+  inline auto operator==(
+      versionbitmap const& lhs, versionbitmap const& rhs) noexcept
+    -> bool
+  {
+    return lhs.equal_impl(rhs);
+  }
 
 } // namespace hello_elements
 } // namespace v13
