f55d1b9fa2ee80da889d88a32c332e9852c5406b
amedama41 <kamo.tanabota@gmail.com>
2016-09-12 22:16:13 +0900

    reimplement v10 port type based on basic_protocol_type

--- include/canard/network/openflow/v10/common/port.hpp
+++ include/canard/network/openflow/v10/common/port.hpp
@@ -4,9 +4,9 @@
 #include <cstdint>
 #include <cstring>
 #include <algorithm>
-#include <boost/operators.hpp>
 #include <boost/utility/string_ref.hpp>
 #include <canard/mac_address.hpp>
+#include <canard/network/openflow/detail/basic_protocol_type.hpp>
 #include <canard/network/openflow/detail/decode.hpp>
 #include <canard/network/openflow/detail/encode.hpp>
 #include <canard/network/openflow/detail/memcmp.hpp>
@@ -21,7 +21,7 @@ namespace v10 {
 
     class port
         : public v10_detail::port_adaptor<port>
-        , private boost::equality_comparable<port>
+        , public detail::basic_protocol_type<port>
     {
     public:
         using raw_ofp_type = v10_detail::ofp_phy_port;
@@ -65,33 +65,31 @@ namespace v10 {
             return port_;
         }
 
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, port_);
-        }
-
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> port
-        {
-            return port{detail::decode<raw_ofp_type>(first, last)};
-        }
-
         static auto from_ofp_port(raw_ofp_type const& ofp_port) noexcept
             -> port
         {
             return port{ofp_port};
         }
 
-        friend auto operator==(port const&, port const&) noexcept
-            -> bool;
-
     private:
+        friend basic_protocol_type;
+
         explicit port(raw_ofp_type const& phy_port) noexcept
             : port_(phy_port)
         {
+        }
+
+        template <class Container>
+        void encode_impl(Container& container) const
+        {
+            detail::encode(container, port_);
+        }
+
+        template <class Iterator>
+        static auto decode_impl(Iterator& first, Iterator last)
+            -> port
+        {
+            return port{detail::decode<raw_ofp_type>(first, last)};
         }
 
         auto equal_impl(port const& rhs) const noexcept
@@ -100,29 +98,23 @@ namespace v10 {
             return detail::memcmp(port_, rhs.port_);
         }
 
+        auto equivalent_impl(port const& rhs) const noexcept
+            -> bool
+        {
+          return port_no() == rhs.port_no()
+              && hardware_address() == rhs.hardware_address()
+              && name() == rhs.name()
+              && config() == rhs.config()
+              && state() == rhs.state()
+              && current_features() == rhs.current_features()
+              && advertised_features() == rhs.advertised_features()
+              && supported_features() == rhs.supported_features()
+              && peer_advertised_features() == rhs.peer_advertised_features();
+        }
+
     private:
         raw_ofp_type port_;
     };
-
-    inline auto operator==(port const& lhs, port const& rhs) noexcept
-        -> bool
-    {
-        return lhs.equal_impl(rhs);
-    }
-
-    inline auto equivalent(port const& lhs, port const& rhs) noexcept
-        -> bool
-    {
-      return lhs.port_no() == rhs.port_no()
-          && lhs.hardware_address() == rhs.hardware_address()
-          && lhs.name() == rhs.name()
-          && lhs.config() == rhs.config()
-          && lhs.state() == rhs.state()
-          && lhs.current_features() == rhs.current_features()
-          && lhs.advertised_features() == rhs.advertised_features()
-          && lhs.supported_features() == rhs.supported_features()
-          && lhs.peer_advertised_features() == rhs.peer_advertised_features();
-    }
 
 } // namespace v10
 } // namespace ofp
