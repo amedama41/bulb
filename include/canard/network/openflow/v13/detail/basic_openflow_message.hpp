4cd4405182d836850cfb49679af5e99c1aeebac6
amedama41 <kamo.tanabota@gmail.com>
2016-09-25 18:34:48 +0900

    reimplement v13 basic_openflow_message type based on basic_protocol_type

--- include/canard/network/openflow/v13/detail/basic_openflow_message.hpp
+++ include/canard/network/openflow/v13/detail/basic_openflow_message.hpp
@@ -1,68 +1,71 @@
-#ifndef CANARD_NET_OFP_V13_BASIC_OPENFLOW_MESSAGE_HPP
-#define CANARD_NET_OFP_V13_BASIC_OPENFLOW_MESSAGE_HPP
+#ifndef CANARD_NET_OFP_DETAIL_V13_BASIC_OPENFLOW_MESSAGE_HPP
+#define CANARD_NET_OFP_DETAIL_V13_BASIC_OPENFLOW_MESSAGE_HPP
 
 #include <cstdint>
-#include <vector>
+#include <stdexcept>
+#include <canard/network/openflow/detail/basic_protocol_type.hpp>
 #include <canard/network/openflow/v13/openflow.hpp>
-#include <canard/network/openflow/v13/detail/transaction_id_assignor.hpp>
 
 namespace canard {
 namespace net {
 namespace ofp {
+namespace detail {
 namespace v13 {
 
-    namespace v13_detail {
+  template <class T>
+  class basic_openflow_message
+    : public detail::basic_protocol_type<T>
+  {
+  private:
+    auto base_header() const noexcept
+      -> ofp::v13::v13_detail::ofp_header const&
+    {
+      return static_cast<T const*>(this)->header();
+    }
 
-        template <class T>
-        class basic_openflow_message
-            : public v13::v13_detail::transaction_id_assignor<std::uint32_t>
-        {
-        private:
-            auto header() const
-                -> v13_detail::ofp_header const&
-            {
-                return static_cast<T const*>(this)->header();
-            }
+  public:
+    static constexpr auto version() noexcept
+      -> std::uint8_t
+    {
+      return ofp::v13::protocol::OFP_VERSION;
+    }
 
-        public:
-            auto version() const
-                -> std::uint8_t
-            {
-                return header().version;
-            }
+    static constexpr auto type() noexcept
+      -> ofp::v13::protocol::ofp_type
+    {
+      return T::message_type;
+    }
 
-            auto type() const
-                -> protocol::ofp_type
-            {
-                return protocol::ofp_type(header().type);
-            }
+    auto length() const noexcept
+      -> std::uint16_t
+    {
+      return base_header().length;
+    }
 
-            auto length() const
-                -> std::uint16_t
-            {
-                return header().length;
-            }
+    auto xid() const noexcept
+      -> std::uint32_t
+    {
+      return base_header().xid;
+    }
 
-            auto xid() const
-                -> std::uint32_t
-            {
-                return header().xid;
-            }
-
-            auto encode() const
-                -> std::vector<unsigned char>
-            {
-                auto buf = std::vector<unsigned char>{};
-                buf.reserve(length());
-                return static_cast<T const*>(this)->encode(buf);
-            }
-        };
-
-    } // namespace v13_detail
+    static void validate_header(ofp::v13::v13_detail::ofp_header const& header)
+    {
+      if (header.version != version()) {
+        throw std::runtime_error{"invalid version"};
+      }
+      if (header.type != type()) {
+        throw std::runtime_error{"invalid message type"};
+      }
+      if (header.length < detail::basic_protocol_type<T>::min_length()) {
+        throw std::runtime_error{"too small length"};
+      }
+    }
+  };
 
 } // namespace v13
+} // namespace detail
 } // namespace ofp
 } // namespace net
 } // namespace canard
 
-#endif // CANARD_NET_OFP_V13_BASIC_OPENFLOW_MESSAGE_HPP
+#endif // CANARD_NET_OFP_DETAIL_V13_BASIC_OPENFLOW_MESSAGE_HPP
