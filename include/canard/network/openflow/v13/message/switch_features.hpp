699d2f52b5e43609ee7b00fb07cb0885cb473bcf
amedama41 <kamo.tanabota@gmail.com>
2016-09-27 19:51:12 +0900

    reimplement v13 switch_features message types based on basic_protocol_type

--- include/canard/network/openflow/v13/message/switch_features.hpp
+++ include/canard/network/openflow/v13/message/switch_features.hpp
@@ -2,7 +2,6 @@
 #define CANARD_NET_OFP_V13_MESSAGES_SWITCH_FEATURES_HPP
 
 #include <cstdint>
-#include <stdexcept>
 #include <canard/network/openflow/detail/decode.hpp>
 #include <canard/network/openflow/detail/encode.hpp>
 #include <canard/network/openflow/get_xid.hpp>
@@ -17,17 +16,19 @@ namespace v13 {
 namespace messages {
 
     class features_request
-        : public v13_detail::basic_openflow_message<features_request>
+        : public detail::v13::basic_openflow_message<features_request>
     {
     public:
         static constexpr protocol::ofp_type message_type
             = protocol::OFPT_FEATURES_REQUEST;
 
+        using raw_ofp_type = v13_detail::ofp_header;
+
         features_request(std::uint32_t const xid = get_xid()) noexcept
             : header_{
-                  protocol::OFP_VERSION
-                , message_type
-                , sizeof(v13_detail::ofp_header)
+                  version()
+                , type()
+                , sizeof(raw_ofp_type)
                 , xid
               }
         {
@@ -39,52 +40,40 @@ namespace messages {
             return header_;
         }
 
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, header_);
-        }
-
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> features_request
-        {
-            auto const header
-                = detail::decode<v13_detail::ofp_header>(first, last);
-            return features_request{header};
-        }
-
-        static void validate(v13_detail::ofp_header const& header)
-        {
-            if (header.version != protocol::OFP_VERSION) {
-                throw std::runtime_error{"invalid version"};
-            }
-            if (header.type != message_type) {
-                throw std::runtime_error{"invalid type"};
-            }
-            if (header.length != sizeof(v13_detail::ofp_header)) {
-                throw std::runtime_error{"invalid length"};
-            }
-        }
-
     private:
-        explicit features_request(v13_detail::ofp_header const& header) noexcept
+        explicit features_request(raw_ofp_type const& header) noexcept
             : header_(header)
         {
         }
 
+        friend basic_protocol_type;
+
+        template <class Container>
+        void encode_impl(Container& container) const
+        {
+            detail::encode(container, header_);
+        }
+
+        template <class Iterator>
+        static auto decode_impl(Iterator& first, Iterator last)
+            -> features_request
+        {
+            return features_request{detail::decode<raw_ofp_type>(first, last)};
+        }
+
     private:
-        v13_detail::ofp_header header_;
+        raw_ofp_type header_;
     };
 
 
     class features_reply
-        : public v13_detail::basic_openflow_message<features_reply>
+        : public detail::v13::basic_openflow_message<features_reply>
     {
     public:
         static constexpr protocol::ofp_type message_type
             = protocol::OFPT_FEATURES_REPLY;
+
+        using raw_ofp_type = v13_detail::ofp_switch_features;
 
         features_reply(features_request const& request
                      , std::uint64_t const dpid
@@ -94,9 +83,9 @@ namespace messages {
                      , std::uint32_t const capabilities) noexcept
             : switch_features_{
                   v13_detail::ofp_header{
-                      protocol::OFP_VERSION
-                    , message_type
-                    , sizeof(v13_detail::ofp_switch_features)
+                      version()
+                    , type()
+                    , sizeof(raw_ofp_type)
                     , request.xid()
                   }
                 , dpid
@@ -146,44 +135,29 @@ namespace messages {
             return switch_features_.capabilities;
         }
 
-        template <class Container>
-        auto encode(Container& container) const
-            -> Container&
-        {
-            return detail::encode(container, switch_features_);
-        }
-
-        template <class Iterator>
-        static auto decode(Iterator& first, Iterator last)
-            -> features_reply
-        {
-            auto const switch_features
-                = detail::decode<v13_detail::ofp_switch_features>(first, last);
-            return features_reply{switch_features};
-        }
-
-        static void validate(v13_detail::ofp_header const& header)
-        {
-            if (header.version != protocol::OFP_VERSION) {
-                throw std::runtime_error{"invalid version"};
-            }
-            if (header.type != message_type) {
-                throw std::runtime_error{"invalid type"};
-            }
-            if (header.length != sizeof(v13_detail::ofp_switch_features)) {
-                throw std::runtime_error{"invalid length"};
-            }
-        }
-
     private:
-        explicit features_reply(
-                v13_detail::ofp_switch_features const& switch_features) noexcept
+        explicit features_reply(raw_ofp_type const& switch_features) noexcept
             : switch_features_(switch_features)
         {
         }
 
+        friend basic_protocol_type;
+
+        template <class Container>
+        void encode_impl(Container& container) const
+        {
+            detail::encode(container, switch_features_);
+        }
+
+        template <class Iterator>
+        static auto decode_impl(Iterator& first, Iterator last)
+            -> features_reply
+        {
+            return features_reply{detail::decode<raw_ofp_type>(first, last)};
+        }
+
     private:
-        v13_detail::ofp_switch_features switch_features_;
+        raw_ofp_type switch_features_;
     };
 
 } // namespace messages
