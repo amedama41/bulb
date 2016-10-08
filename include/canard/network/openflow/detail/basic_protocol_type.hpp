9a9c198ece6018c4f7b072e9c72adc2b2578c7a6
amedama41 <kamo.tanabota@gmail.com>
2016-10-08 16:19:25 +0900

    add functions related to trailing padding into basic_protocol_type

--- include/canard/network/openflow/detail/basic_protocol_type.hpp
+++ include/canard/network/openflow/detail/basic_protocol_type.hpp
@@ -2,8 +2,11 @@
 #define CANARD_NET_OFP_DETAIL_BASIC_PROTOCOL_TYPE_HPP
 
 #include <cstdint>
+#include <iterator>
+#include <type_traits>
 #include <utility>
 #include <boost/operators.hpp>
+#include <canard/network/openflow/detail/encode.hpp>
 #include <canard/network/openflow/validator.hpp>
 
 namespace canard {
@@ -11,12 +14,109 @@ namespace net {
 namespace ofp {
 namespace detail {
 
-  template <class T>
-  constexpr auto get_min_length(T*) noexcept
-    -> std::uint16_t
-  {
-    return sizeof(typename T::raw_ofp_type);
-  };
+  namespace bpt_detail {
+
+    template <class T>
+    constexpr auto get_min_length(T*) noexcept
+      -> std::uint16_t
+    {
+      return sizeof(typename T::raw_ofp_type);
+    };
+
+    template <class T>
+    constexpr auto get_min_length_impl() noexcept
+      -> std::uint16_t
+    {
+      return get_min_length(static_cast<T*>(nullptr));
+    }
+
+    constexpr auto exclude_padding(...) noexcept
+      -> bool
+    {
+      return false;
+    }
+
+    template <class T>
+    using does_length_exclude_padding
+      = std::integral_constant<bool, exclude_padding(static_cast<T*>(nullptr))>;
+
+    constexpr auto exact_length(std::uint16_t const length) noexcept
+      -> std::uint16_t
+    {
+      return (length + 7) / 8 * 8;
+    }
+
+    constexpr auto padding_length(std::uint16_t const length) noexcept
+      -> std::uint16_t
+    {
+      return bpt_detail::exact_length(length) - length;
+    }
+
+    template <class Container, class T>
+    void optionally_encode_padding_impl(
+        Container& container, T const& t, std::true_type)
+    {
+      constexpr std::uint8_t const padding[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+      detail::encode_byte_array(
+          container, padding, bpt_detail::padding_length(t.length()));
+    }
+
+    template <class Container, class T>
+    void optionally_encode_padding_impl(
+        Container&, T const&, std::false_type) noexcept
+    {
+    }
+
+    template <class Container, class T>
+    void optionally_encode_padding(Container& container, T const& t)
+    {
+      bpt_detail::optionally_encode_padding_impl(
+          container, t, does_length_exclude_padding<T>{});
+    }
+
+    template <class Iterator, class T>
+    void optionally_decode_padding_impl(
+        Iterator& first, Iterator, T const& t, std::true_type)
+    {
+      std::advance(first, bpt_detail::padding_length(t.length()));
+    }
+
+    template <class Iterator, class T>
+    void optionally_decode_padding_impl(
+        Iterator&, Iterator, T const&, std::false_type) noexcept
+    {
+    }
+
+    template <class Iterator, class T>
+    void optionally_decode_padding(Iterator& first, Iterator last, T const& t)
+    {
+      bpt_detail::optionally_decode_padding_impl(
+          first, last, t, does_length_exclude_padding<T>{});
+    }
+
+    constexpr auto calc_byte_length_impl(
+        std::uint16_t const length, std::true_type) noexcept
+      -> std::uint16_t
+    {
+      return bpt_detail::exact_length(length);
+    }
+
+    constexpr auto calc_byte_length_impl(
+        std::uint16_t const length, std::false_type) noexcept
+      -> std::uint16_t
+    {
+      return length;
+    }
+
+    template <class T>
+    constexpr auto calc_byte_length(std::uint16_t const length) noexcept
+      -> std::uint16_t
+    {
+      return bpt_detail::calc_byte_length_impl(
+          length, does_length_exclude_padding<T>{});
+    }
+
+  } // namespace bpt_detail
 
   template <class T>
   class basic_protocol_type
@@ -26,20 +126,33 @@ namespace detail {
     static constexpr auto min_length() noexcept
       -> std::uint16_t
     {
-      return get_min_length(static_cast<T*>(nullptr));
+      return bpt_detail::get_min_length_impl<T>();
+    }
+
+    static constexpr auto min_byte_length() noexcept
+      -> std::uint16_t
+    {
+      return bpt_detail::calc_byte_length<T>(min_length());
+    }
+
+    auto byte_length() const noexcept
+      -> std::uint16_t
+    {
+      return bpt_detail::calc_byte_length<T>(derived().length());
     }
 
     template <class Validator>
     void validate(Validator&& validator) const
     {
-      derived()->validate_impl(std::forward<Validator>(validator));
+      derived().validate_impl(std::forward<Validator>(validator));
     }
 
     template <class Container>
     auto encode(Container& container) const
       -> Container&
     {
-      derived()->encode_impl(container);
+      derived().encode_impl(container);
+      bpt_detail::optionally_encode_padding(container, derived());
       return container;
     }
 
@@ -47,7 +160,9 @@ namespace detail {
     static auto decode(Iterator& first, Iterator last)
       -> T
     {
-      return T::decode_impl(first, last);
+      auto t = T::decode_impl(first, last);
+      bpt_detail::optionally_decode_padding(first, last, t);
+      return t;
     }
 
     template <class... Args>
@@ -73,9 +188,9 @@ namespace detail {
 
   private:
     auto derived() const noexcept
-      -> T const*
+      -> T const&
     {
-      return static_cast<T const*>(this);
+      return static_cast<T const&>(*this);
     }
   };
 
