3aa9a46210773117e4071f02f53954b7e32e83d3
amedama41 <kamo.tanabota@gmail.com>
2016-10-21 20:48:36 +0900

    use ofp list for action_list

--- include/canard/network/openflow/v13/action_list.hpp
+++ include/canard/network/openflow/v13/action_list.hpp
@@ -3,7 +3,7 @@
 
 #include <canard/network/openflow/detail/config.hpp>
 
-#include <canard/network/openflow/detail/any_list.hpp>
+#include <canard/network/openflow/list.hpp>
 #include <canard/network/openflow/v13/any_action.hpp>
 
 namespace canard {
@@ -11,7 +11,7 @@ namespace net {
 namespace ofp {
 namespace v13 {
 
-    using action_list = detail::any_list<any_action>;
+  using action_list = ofp::list<any_action>;
 
 } // namespace v13
 } // namespace ofp
@@ -24,11 +24,9 @@ namespace v13 {
 namespace canard {
 namespace net {
 namespace ofp {
-namespace detail {
 
-    extern template class any_list<ofp::v13::any_action>;
+  extern template class list<ofp::v13::any_action>;
 
-} // namespace detail
 } // namespace ofp
 } // namespace net
 } // namespace canard
--- include/canard/network/openflow/v13/impl/action_list.ipp
+++ include/canard/network/openflow/v13/impl/action_list.ipp
@@ -2,21 +2,19 @@
 #define CANARD_NET_OFP_V13_IMPL_ACTION_LIST_IPP
 
 #include <canard/network/openflow/detail/config.hpp>
+#include <canard/network/openflow/v13/action_list.hpp>
 
 #if !defined(CANARD_NET_OFP_HEADER_ONLY)
 # if defined(CANARD_NET_OFP_USE_EXPLICIT_INSTANTIATION)
 
-#  include <canard/network/openflow/detail/impl/any_list.hpp>
-#  include <canard/network/openflow/v13/action_list.hpp>
+#  include <canard/network/openflow/detail/impl/list.hpp>
 
 namespace canard {
 namespace net {
 namespace ofp {
-namespace detail {
 
-  template class any_list<ofp::v13::any_action>;
+  template class list<ofp::v13::any_action>;
 
-} // namespace detail
 } // namespace ofp
 } // namespace net
 } // namespace canard
