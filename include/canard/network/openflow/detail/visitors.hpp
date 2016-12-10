a7994bfc26451276040acf6237d9574fff078e95
amedama41 <kamo.tanabota@gmail.com>
2016-12-10 21:02:54 +0900

    remove property_visitor

--- include/canard/network/openflow/detail/visitors.hpp
+++ include/canard/network/openflow/detail/visitors.hpp
@@ -88,18 +88,6 @@ namespace ofp {
             }
         };
 
-        class property_visitor
-            : public boost::static_visitor<std::uint16_t>
-        {
-        public:
-            template <class T>
-            auto operator()(T const& t) const
-                -> std::uint16_t
-            {
-                return t.property();
-            }
-        };
-
         class equivalent_visitor
             : public boost::static_visitor<bool>
         {
