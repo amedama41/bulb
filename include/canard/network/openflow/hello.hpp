2597c0963b2c9fc60ba5759b636c6b65af76e93e
amedama41 <kamo.tanabota@gmail.com>
2017-02-11 17:01:58 +0900

    remove read_ofp_header

--- include/canard/network/openflow/hello.hpp
+++ include/canard/network/openflow/hello.hpp
@@ -1,11 +1,6 @@
 #ifndef CANARD_NET_OFP_HELLO_HPP
 #define CANARD_NET_OFP_HELLO_HPP
 
-#include <cstdint>
-#include <cstring>
-#include <boost/asio/buffer.hpp>
-#include <boost/endian/conversion.hpp>
-#include <canard/network/openflow/v10/openflow.hpp>
 #include <canard/network/openflow/v13/hello_elements.hpp>
 #include <canard/network/openflow/v13/openflow.hpp>
 #include <canard/network/openflow/v13/message/hello.hpp>
@@ -18,19 +13,6 @@ namespace ofp {
     using hello = v13::hello;
     using ofp_header = v13::protocol::ofp_header;
     namespace hello_elements = v13::hello_elements;
-
-    namespace detail {
-
-        auto read_ofp_header(boost::asio::const_buffer const& buffer)
-            -> ofp_header
-        {
-            auto header = ofp_header{};
-            std::memcpy(&header, boost::asio::buffer_cast<std::uint8_t const*>(buffer), sizeof(header));
-            boost::endian::big_to_native_inplace(header);
-            return header;
-        }
-
-    } // namespace detail
 
 } // namespace ofp
 } // namespace net
