8906aad0ba2db70a39d23b5bbd8dbfa72646aa15
amedama41 <kamo.tanabota@gmail.com>
2017-02-17 21:08:52 +0900

    add oxm_header_ops

--- include/canard/network/openflow/v13/common/oxm_header.hpp
+++ include/canard/network/openflow/v13/common/oxm_header.hpp
@@ -12,11 +12,71 @@ namespace net {
 namespace ofp {
 namespace v13 {
 
+  namespace oxm_header_ops {
+
+    using oxm_header_type = std::uint32_t;
+
+    constexpr auto oxm_header(
+          std::uint32_t const oxm_class
+        , std::uint8_t const oxm_field
+        , std::uint8_t const oxm_length) noexcept
+      -> oxm_header_type
+    {
+      return std::uint32_t(oxm_class << 16)
+           | std::uint32_t(oxm_field << 9)
+           | std::uint32_t(oxm_length);
+    }
+
+    constexpr auto oxm_header_w(
+          std::uint32_t const oxm_class
+        , std::uint8_t const oxm_field
+        , std::uint8_t const oxm_length) noexcept
+      -> oxm_header_type
+    {
+      return std::uint32_t(oxm_class << 16)
+           | std::uint32_t(oxm_field << 9)
+           | std::uint32_t(0x00000100)
+           | std::uint32_t(oxm_length);
+    }
+
+    constexpr auto oxm_class(oxm_header_type const oxm_header) noexcept
+      -> std::uint16_t
+    {
+      return oxm_header >> 16;
+    }
+
+    constexpr auto oxm_field(oxm_header_type const oxm_header) noexcept
+      -> std::uint8_t
+    {
+      return (oxm_header >> 9) & 0x7f;
+    }
+
+    constexpr auto oxm_type(oxm_header_type const oxm_header) noexcept
+      -> std::uint32_t
+    {
+      return oxm_header >> 9;
+    }
+
+    constexpr auto oxm_hasmask(oxm_header_type const oxm_header) noexcept
+      -> bool
+    {
+      return oxm_header & 0x00000100;
+    }
+
+    constexpr auto oxm_length(oxm_header_type const oxm_header) noexcept
+      -> std::uint8_t
+    {
+      return oxm_header & 0x000000ff;
+    }
+
+  } // namespace oxm_header_ops
+
+
   class oxm_header
     : public detail::basic_protocol_type<oxm_header>
   {
   public:
-    using raw_ofp_type = std::uint32_t;
+    using raw_ofp_type = oxm_header_ops::oxm_header_type;
 
     constexpr explicit oxm_header(std::uint32_t const oxm_header) noexcept
       : oxm_header_(oxm_header)
@@ -26,31 +86,31 @@ namespace v13 {
     constexpr auto oxm_class() const noexcept
       -> std::uint16_t
     {
-      return oxm_header_ >> 16;
+      return oxm_header_ops::oxm_class(oxm_header_);
     }
 
     constexpr auto oxm_field() const noexcept
       -> std::uint8_t
     {
-      return (oxm_header_ >> 9) & 0x7f;
+      return oxm_header_ops::oxm_field(oxm_header_);
     }
 
     constexpr auto oxm_type() const noexcept
       -> std::uint32_t
     {
-      return oxm_header_ >> 9;
+      return oxm_header_ops::oxm_type(oxm_header_);
     }
 
     constexpr auto oxm_hasmask() const noexcept
       -> bool
     {
-      return oxm_header_ & 0x00000100;
+      return oxm_header_ops::oxm_hasmask(oxm_header_);
     }
 
     constexpr auto oxm_length() const noexcept
       -> std::uint8_t
     {
-      return oxm_header_ & 0x000000ff;
+      return oxm_header_ops::oxm_length(oxm_header_);
     }
 
     constexpr auto to_ofp_type() const noexcept
@@ -83,7 +143,7 @@ namespace v13 {
     static auto decode_impl(Iterator& first, Iterator last)
       -> oxm_header
     {
-      return oxm_header{detail::decode<std::uint32_t>(first, last)};
+      return oxm_header{detail::decode<raw_ofp_type>(first, last)};
     }
 
     auto equal_impl(oxm_header const& rhs) const noexcept
@@ -99,7 +159,7 @@ namespace v13 {
     }
 
   private:
-    std::uint32_t oxm_header_;
+    raw_ofp_type oxm_header_;
   };
 
 } // namespace v13
