7e729b154cece2e257623d81d3b306d1b3ac19f5
amedama41 <kamo.tanabota@gmail.com>
2017-04-17 19:10:19 +0900

    add equality test for v13 packet_out type

--- test/v13/message/packet_out_test.cpp
+++ test/v13/message/packet_out_test.cpp
@@ -9,241 +9,234 @@ namespace v13 = of::v13;
 namespace match = v13::oxm_match_fields;
 namespace protocol = v13::protocol;
 
+namespace {
+
+  constexpr auto ofp_pkt_out_size = sizeof(protocol::ofp_packet_out);
+
+  struct packet_out_parameter {
+    v13::messages::packet_out::data_type data = "\x10\x11\x12\x13\x14\x15"_bbin;
+    std::uint32_t in_port = protocol::OFPP_CONTROLLER;
+    v13::actions::push_vlan push_vlan{0x8100};
+    v13::actions::set_vlan_vid set_vlan_vid{protocol::OFPVID_PRESENT | 3};
+    v13::actions::output output{3};
+    v13::action_list actions{push_vlan, set_vlan_vid, output};
+    std::uint32_t xid = 0x1234;
+  };
+  struct packet_out_fixture : packet_out_parameter {
+    v13::messages::packet_out sut{data, in_port, actions, xid};
+    std::vector<std::uint8_t> bin
+      = "\x04\x0d\x00\x46\x00\x00\x12\x34" "\xff\xff\xff\xff\xff\xff\xff\xfd"
+        "\x00\x28\x00\x00\x00\x00\x00\x00"
+        "\x00\x11\x00\x08\x81\x00\x00\x00" "\x00\x19\x00\x10\x80\x00\x0c\x02"
+        "\x10\x03\x00\x00\x00\x00\x00\x00" "\x00\x00\x00\x10\x00\x00\x00\x03"
+        "\xff\xff\x00\x00\x00\x00\x00\x00"
+        "\x10\x11\x12\x13\x14\x15"_bin
+        ;
+  };
+}
+
 BOOST_AUTO_TEST_SUITE(message_test)
-
-BOOST_AUTO_TEST_SUITE(packet_out_test)
-
-    constexpr auto ofp_pkt_out_size = sizeof(protocol::ofp_packet_out);
-
-    BOOST_AUTO_TEST_CASE(constructor_from_buffer_id_test)
+BOOST_AUTO_TEST_SUITE(packet_out)
+  BOOST_AUTO_TEST_SUITE(constructor)
+    BOOST_AUTO_TEST_CASE(constructible_from_buffer_id)
     {
-        auto const buffer_id = std::uint32_t{0xffffff};
-        auto const in_port = 1;
-        auto const actions = v13::action_list{
-            v13::actions::decrement_nw_ttl{}, v13::actions::output{3}
-        };
-        auto const xid = std::uint32_t{0xff00ff};
+      auto const buffer_id = std::uint32_t{0xffffff};
+      auto const in_port = 1;
+      auto const actions = v13::action_list{
+        v13::actions::decrement_nw_ttl{}, v13::actions::output{3}
+      };
+      auto const xid = std::uint32_t{0xff00ff};
 
-        auto const sut = v13::messages::packet_out{
-            buffer_id, in_port, actions, xid
-        };
+      v13::messages::packet_out const sut{buffer_id, in_port, actions, xid};
 
-        BOOST_TEST(sut.version() == protocol::OFP_VERSION);
-        BOOST_TEST(sut.type() == protocol::OFPT_PACKET_OUT);
-        BOOST_TEST(sut.length() == ofp_pkt_out_size + actions.length());
-        BOOST_TEST(sut.xid() == xid);
-        BOOST_TEST(sut.buffer_id() == buffer_id);
-        BOOST_TEST(sut.in_port() == in_port);
-        BOOST_TEST(sut.actions_length() == 24);
-        BOOST_TEST(sut.frame_length() == 0);
-        BOOST_TEST(sut.frame().empty());
+      BOOST_TEST(sut.version() == protocol::OFP_VERSION);
+      BOOST_TEST(sut.type() == protocol::OFPT_PACKET_OUT);
+      BOOST_TEST(sut.length() == ofp_pkt_out_size + actions.length());
+      BOOST_TEST(sut.xid() == xid);
+      BOOST_TEST(sut.buffer_id() == buffer_id);
+      BOOST_TEST(sut.in_port() == in_port);
+      BOOST_TEST(sut.actions_length() == 24);
+      BOOST_TEST(sut.frame_length() == 0);
+      BOOST_TEST(sut.frame().empty());
     }
-
-    BOOST_AUTO_TEST_CASE(constructor_from_binary_test)
+    BOOST_AUTO_TEST_CASE(constructible_from_binary)
     {
-        auto const data = "\x00\x01\x02\x03\x04\x05"_bbin;
-        auto const in_port = protocol::OFPP_CONTROLLER;
-        auto const actions = v13::action_list{
-              v13::actions::push_vlan{0x8100}
-            , v13::actions::set_vlan_vid{3}
-            , v13::actions::output{3}
-        };
-        auto const xid = std::uint32_t{0x000001};
+      auto const data = "\x00\x01\x02\x03\x04\x05"_bbin;
+      auto const in_port = protocol::OFPP_CONTROLLER;
+      auto const actions = v13::action_list{
+          v13::actions::push_vlan{0x8100}
+        , v13::actions::set_vlan_vid{3}
+        , v13::actions::output{3}
+      };
+      auto const xid = std::uint32_t{0x000001};
 
-        auto const sut = v13::messages::packet_out{
-            data, in_port, actions, xid
-        };
+      v13::messages::packet_out const sut{data, in_port, actions, xid};
 
-        BOOST_TEST(sut.length() == ofp_pkt_out_size + actions.length() + data.size());
-        BOOST_TEST(sut.xid() == xid);
-        BOOST_TEST(sut.buffer_id() == protocol::OFP_NO_BUFFER);
-        BOOST_TEST(sut.in_port() == in_port);
-        BOOST_TEST(sut.actions_length() == 40);
-        BOOST_TEST(sut.frame_length() == data.size());
-        BOOST_TEST(sut.frame() == data, boost::test_tools::per_element{});
+      BOOST_TEST(
+          sut.length() == ofp_pkt_out_size + actions.length() + data.size());
+      BOOST_TEST(sut.xid() == xid);
+      BOOST_TEST(sut.buffer_id() == protocol::OFP_NO_BUFFER);
+      BOOST_TEST(sut.in_port() == in_port);
+      BOOST_TEST(sut.actions_length() == 40);
+      BOOST_TEST(sut.frame_length() == data.size());
+      BOOST_TEST(sut.frame() == data, boost::test_tools::per_element{});
     }
-
-    BOOST_AUTO_TEST_CASE(constructor_from_empty_actions_test)
+    BOOST_AUTO_TEST_CASE(constructible_from_empty_actions)
     {
-        auto const data = "\x10\x11\x12\x13\x14\x15"_bbin;
-        auto const in_port = 2;
-        auto const xid = std::uint32_t{0x000001};
+      auto const data = "\x10\x11\x12\x13\x14\x15"_bbin;
+      auto const in_port = 2;
+      auto const xid = std::uint32_t{0x000001};
 
-        auto const sut = v13::messages::packet_out{
-            data, in_port, {}, xid
-        };
+      v13::messages::packet_out const sut{data, in_port, {}, xid};
 
-        BOOST_TEST(sut.length() == ofp_pkt_out_size + data.size());
-        BOOST_TEST(sut.xid() == xid);
-        BOOST_TEST(sut.buffer_id() == protocol::OFP_NO_BUFFER);
-        BOOST_TEST(sut.in_port() == in_port);
-        BOOST_TEST(sut.actions_length() == 0);
-        BOOST_TEST(sut.frame_length() == data.size());
-        BOOST_TEST(sut.frame() == data, boost::test_tools::per_element{});
+      BOOST_TEST(sut.length() == ofp_pkt_out_size + data.size());
+      BOOST_TEST(sut.xid() == xid);
+      BOOST_TEST(sut.buffer_id() == protocol::OFP_NO_BUFFER);
+      BOOST_TEST(sut.in_port() == in_port);
+      BOOST_TEST(sut.actions_length() == 0);
+      BOOST_TEST(sut.frame_length() == data.size());
+      BOOST_TEST(sut.frame() == data, boost::test_tools::per_element{});
     }
-
-    BOOST_AUTO_TEST_CASE(constructor_from_empty_binary_test)
+    BOOST_AUTO_TEST_CASE(constructible_from_empty_binary)
     {
-        auto const in_port = 2;
+      auto const in_port = 2;
 
-        auto const sut = v13::messages::packet_out{
-            v13::messages::packet_out::data_type{}, in_port, {}
-        };
+      v13::messages::packet_out const sut{
+        v13::messages::packet_out::data_type{}, in_port, {}
+      };
 
-        BOOST_TEST(sut.length() == ofp_pkt_out_size);
-        BOOST_TEST(sut.buffer_id() == protocol::OFP_NO_BUFFER);
-        BOOST_TEST(sut.in_port() == in_port);
-        BOOST_TEST(sut.actions_length() == 0);
-        BOOST_TEST(sut.frame_length() == 0);
-        BOOST_TEST(sut.frame().empty());
+      BOOST_TEST(sut.length() == ofp_pkt_out_size);
+      BOOST_TEST(sut.buffer_id() == protocol::OFP_NO_BUFFER);
+      BOOST_TEST(sut.in_port() == in_port);
+      BOOST_TEST(sut.actions_length() == 0);
+      BOOST_TEST(sut.frame_length() == 0);
+      BOOST_TEST(sut.frame().empty());
     }
-
-    BOOST_AUTO_TEST_CASE(copy_constructor_from_empty_binary_test)
+    BOOST_AUTO_TEST_CASE(copy_constructible_from_no_data_packet_out)
     {
-        auto const sut = v13::messages::packet_out{
-            0, 0, {v13::actions::output::to_controller()}
-        };
+      auto const sut = v13::messages::packet_out{
+        0, 0, {v13::actions::output::to_controller()}
+      };
 
-        auto const copy = sut;
+      auto const copy = sut;
 
-        BOOST_TEST(copy.version() == sut.version());
-        BOOST_TEST(copy.type() == sut.type());
-        BOOST_TEST(copy.length() == sut.length());
-        BOOST_TEST(copy.xid() == sut.xid());
-        BOOST_TEST(copy.buffer_id() == sut.buffer_id());
-        BOOST_TEST(copy.in_port() == sut.in_port());
-        BOOST_TEST(copy.actions_length() == sut.actions_length());
-        BOOST_TEST(copy.frame_length() == sut.frame_length());
-        BOOST_TEST((copy.frame() == sut.frame()));
+      BOOST_TEST((copy == sut));
     }
-
-    struct packet_out_fixture
+    BOOST_FIXTURE_TEST_CASE(copy_constructible, packet_out_fixture)
     {
-        v13::messages::packet_out sut = v13::messages::packet_out{
-              "\x10\x11\x12\x13\x14\x15"_bbin
-            , protocol::OFPP_CONTROLLER
-            , v13::action_list{
-                  v13::actions::push_vlan{0x8100}
-                , v13::actions::set_vlan_vid{protocol::OFPVID_PRESENT | 3}
-                , v13::actions::output{3}
-              }
-            , 0x1234
-        };
-    };
+      auto const copy = sut;
 
-    BOOST_FIXTURE_TEST_CASE(copy_constructor_test, packet_out_fixture)
-    {
-        auto const copy = sut;
-
-        BOOST_TEST(copy.length() == sut.length());
-        BOOST_TEST(copy.xid() == sut.xid());
-        BOOST_TEST(copy.buffer_id() == sut.buffer_id());
-        BOOST_TEST(copy.in_port() == sut.in_port());
-        BOOST_TEST(copy.actions_length() == sut.actions_length());
-        BOOST_TEST(copy.frame_length() == sut.frame_length());
-        BOOST_TEST((copy.frame() == sut.frame()));
+      BOOST_TEST((copy == sut));
     }
-
-    BOOST_FIXTURE_TEST_CASE(move_constructor_test, packet_out_fixture)
+    BOOST_FIXTURE_TEST_CASE(move_constructible, packet_out_fixture)
     {
-        auto const org = sut;
+      auto moved = sut;
 
-        auto const copy = std::move(sut);
+      auto const copy = std::move(moved);
 
-        BOOST_TEST(copy.length() == org.length());
-        BOOST_TEST(copy.xid() == org.xid());
-        BOOST_TEST(copy.buffer_id() == org.buffer_id());
-        BOOST_TEST(copy.in_port() == org.in_port());
-        BOOST_TEST(copy.actions_length() == org.actions_length());
-        BOOST_TEST(copy.frame_length() == org.frame_length());
-        BOOST_TEST((copy.frame() == org.frame()));
-
-        BOOST_TEST(sut.length() == ofp_pkt_out_size);
-        BOOST_TEST(sut.actions_length() == 0);
-        BOOST_TEST(sut.frame_length() == 0);
-        BOOST_TEST(sut.frame().empty());
+      BOOST_TEST((copy == sut));
+      BOOST_TEST(moved.length() == ofp_pkt_out_size);
+      BOOST_TEST(moved.actions_length() == 0);
+      BOOST_TEST(moved.frame_length() == 0);
+      BOOST_TEST(moved.frame().empty());
     }
+  BOOST_AUTO_TEST_SUITE_END() // constructor
 
-    BOOST_FIXTURE_TEST_CASE(copy_assignment_test, packet_out_fixture)
+  BOOST_FIXTURE_TEST_SUITE(assignment, packet_out_fixture)
+    BOOST_AUTO_TEST_CASE(copy_assignable)
     {
-        auto copy = v13::messages::packet_out{0, 0, {}};
+      auto copy = v13::messages::packet_out{0, 0, {}};
 
-        copy = sut;
+      copy = sut;
 
-        BOOST_TEST(copy.length() == sut.length());
-        BOOST_TEST(copy.xid() == sut.xid());
-        BOOST_TEST(copy.buffer_id() == sut.buffer_id());
-        BOOST_TEST(copy.in_port() == sut.in_port());
-        BOOST_TEST(copy.actions_length() == sut.actions_length());
-        BOOST_TEST(copy.frame_length() == sut.frame_length());
-        BOOST_TEST((copy.frame() == sut.frame()));
+      BOOST_TEST((copy == sut));
     }
-
-    BOOST_FIXTURE_TEST_CASE(move_assignment_test, packet_out_fixture)
+    BOOST_AUTO_TEST_CASE(move_assignable)
     {
-        auto const org = sut;
-        auto copy = v13::messages::packet_out{0, 0, {}};
+      auto moved = sut;
+      auto copy = v13::messages::packet_out{0, 0, {}};
 
-        copy = std::move(sut);
+      copy = std::move(moved);
 
-        BOOST_TEST(copy.length() == org.length());
-        BOOST_TEST(copy.xid() == org.xid());
-        BOOST_TEST(copy.buffer_id() == org.buffer_id());
-        BOOST_TEST(copy.in_port() == org.in_port());
-        BOOST_TEST(copy.actions_length() == org.actions_length());
-        BOOST_TEST(copy.frame_length() == org.frame_length());
-        BOOST_TEST((copy.frame() == org.frame()));
-
-        BOOST_TEST(sut.length() == ofp_pkt_out_size);
-        BOOST_TEST(sut.actions_length() == 0);
-        BOOST_TEST(sut.frame_length() == 0);
-        BOOST_TEST(sut.frame().empty());
+      BOOST_TEST((copy == sut));
+      BOOST_TEST(moved.length() == ofp_pkt_out_size);
+      BOOST_TEST(moved.actions_length() == 0);
+      BOOST_TEST(moved.frame_length() == 0);
+      BOOST_TEST(moved.frame().empty());
     }
+  BOOST_AUTO_TEST_SUITE_END() // assignment
 
-    BOOST_FIXTURE_TEST_CASE(encode_test, packet_out_fixture)
+  BOOST_FIXTURE_TEST_SUITE(equality, packet_out_parameter)
+    BOOST_AUTO_TEST_CASE(true_if_same_object)
     {
-        auto buffer = std::vector<unsigned char>{};
+      auto const sut = v13::messages::packet_out{data, in_port, actions, xid};
 
-        sut.encode(buffer);
-
-        BOOST_TEST(buffer.size() == sut.length());
-
-        auto const expected
-            = "\x04\x0d\x00\x46\x00\x00\x12\x34" "\xff\xff\xff\xff\xff\xff\xff\xfd"
-              "\x00\x28\x00\x00\x00\x00\x00\x00" "\x00\x11\x00\x08\x81\x00\x00\x00"
-              "\x00\x19\x00\x10\x80\x00\x0c\x02" "\x10\x03\x00\x00\x00\x00\x00\x00"
-              "\x00\x00\x00\x10\x00\x00\x00\x03" "\xff\xff\x00\x00\x00\x00\x00\x00"
-              "\x10\x11\x12\x13\x14\x15"_bin
-            ;
-        BOOST_TEST(buffer == expected, boost::test_tools::per_element{});
+      BOOST_TEST((sut == sut));
     }
-
-    BOOST_FIXTURE_TEST_CASE(decode_test, packet_out_fixture)
+    BOOST_AUTO_TEST_CASE(true_if_values_are_equal)
     {
-        auto const buffer
-            = "\x04\x0d\x00\x46\x00\x00\x12\x34" "\xff\xff\xff\xff\xff\xff\xff\xfd"
-              "\x00\x28\x00\x00\x00\x00\x00\x00" "\x00\x11\x00\x08\x81\x00\x00\x00"
-              "\x00\x19\x00\x10\x80\x00\x0c\x02" "\x10\x03\x00\x00\x00\x00\x00\x00"
-              "\x00\x00\x00\x10\x00\x00\x00\x03" "\xff\xff\x00\x00\x00\x00\x00\x00"
-              "\x10\x11\x12\x13\x14\x15"_bin
-            ;
-
-        auto it = buffer.begin();
-        auto const it_end = buffer.end();
-        auto pkt_out = v13::messages::packet_out::decode(it, it_end);
-
-        BOOST_TEST(sut.version() == pkt_out.version());
-        BOOST_TEST(sut.type() == pkt_out.type());
-        BOOST_TEST(sut.length() == pkt_out.length());
-        BOOST_TEST(sut.xid() == pkt_out.xid());
-        BOOST_TEST(sut.buffer_id() == pkt_out.buffer_id());
-        BOOST_TEST(sut.in_port() == pkt_out.in_port());
-        BOOST_TEST(sut.actions_length() == pkt_out.actions_length());
-        BOOST_TEST(sut.frame_length() == pkt_out.frame_length());
-        BOOST_TEST(sut.frame() == pkt_out.frame(), boost::test_tools::per_element{});
+      BOOST_TEST(
+          (v13::messages::packet_out{data, in_port, actions, xid}
+        == v13::messages::packet_out{data, in_port, actions, xid}));
     }
+    BOOST_AUTO_TEST_CASE(false_if_data_is_not_equal)
+    {
+      BOOST_TEST(
+          (v13::messages::packet_out{"1"_bbin, in_port, actions, xid}
+        != v13::messages::packet_out{"2"_bbin, in_port, actions, xid}));
+    }
+    BOOST_AUTO_TEST_CASE(false_if_buffer_id_is_not_equal)
+    {
+      BOOST_TEST(
+          (v13::messages::packet_out{1, in_port, actions, xid}
+        != v13::messages::packet_out{2, in_port, actions, xid}));
+    }
+    BOOST_AUTO_TEST_CASE(false_if_in_port_is_not_equal)
+    {
+      BOOST_TEST(
+          (v13::messages::packet_out{data, 1, actions, xid}
+        != v13::messages::packet_out{data, 2, actions, xid}));
+    }
+    BOOST_AUTO_TEST_CASE(false_if_actions_is_not_equal)
+    {
+      auto set_tcp_src1 = v13::actions::set_tcp_src{1};
+      auto set_tcp_src2 = v13::actions::set_tcp_src{2};
 
-BOOST_AUTO_TEST_SUITE_END() // packet_out_test
+      BOOST_TEST(
+          (v13::messages::packet_out{data, in_port, {set_tcp_src1}, xid}
+        != v13::messages::packet_out{data, in_port, {set_tcp_src2}, xid}));
+    }
+    BOOST_AUTO_TEST_CASE(false_if_xid_is_not_equal)
+    {
+      BOOST_TEST(
+          (v13::messages::packet_out{data, in_port, actions, 1}
+        != v13::messages::packet_out{data, in_port, actions, 2}));
+    }
+  BOOST_AUTO_TEST_SUITE_END() // equality
 
+  BOOST_AUTO_TEST_SUITE(encode)
+    BOOST_FIXTURE_TEST_CASE(generate_binary, packet_out_fixture)
+    {
+      auto buffer = std::vector<unsigned char>{};
+
+      sut.encode(buffer);
+
+      BOOST_TEST(buffer.size() == sut.byte_length());
+      BOOST_TEST(buffer == bin, boost::test_tools::per_element{});
+    }
+  BOOST_AUTO_TEST_SUITE_END() // encode
+
+  BOOST_AUTO_TEST_SUITE(decode)
+    BOOST_FIXTURE_TEST_CASE(constructible_from_binary, packet_out_fixture)
+    {
+      auto it = bin.begin();
+
+      auto pkt_out = v13::messages::packet_out::decode(it, bin.end());
+
+      BOOST_TEST((it == bin.end()));
+      BOOST_TEST((sut == pkt_out));
+    }
+  BOOST_AUTO_TEST_SUITE_END() // decode
+BOOST_AUTO_TEST_SUITE_END() // packet_out
 BOOST_AUTO_TEST_SUITE_END() // message_test
 
